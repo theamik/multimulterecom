78e6639f2e5d238b6124c6128bd903022ec0106b
theamik <amik.sylhet@gmail.com>
2024-01-25 21:59:55 +0600

    first commit

---
+++ backend/db/Database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    })
+    .then((data) => {
+      // console.log(`mongod connected with server: ${data.connection.host}`);
+      console.log(`mongod connected with server: ${process.env.DB_URL}`);
+    });
+};
+
+module.exports = connectDatabase;
